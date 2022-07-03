e33dbc3d13169bcf4fb33444f8024dd413333858
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-04 00:10:08 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#import beatifull soup library
+from bs4 import BeautifulSoup
+#create html document
+html_doc = """
+    <html>
+        <head>
+            <title>My First Page</title>
+        </head>
+        <body>
+            <h2>My First Heading</h2>
+            <p>My first paragraph.</p>
+        </body>
+    </html>
+""";
+
+#parse html document
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+#view the soup type
+print(type(soup))
